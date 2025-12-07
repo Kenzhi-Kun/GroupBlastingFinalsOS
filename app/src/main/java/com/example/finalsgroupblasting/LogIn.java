63ebc9078edf5e40ba0ea648b81f07ffe78313c9
Kenzhi <ekce2252765@slu.edu.ph>
2025-12-07 11:13:17 +0800

    Login Authenticator

--- app/src/main/java/com/example/finalsgroupblasting/LogIn.java
+++ app/src/main/java/com/example/finalsgroupblasting/LogIn.java
@@ -1,5 +1,6 @@
 package com.example.finalsgroupblasting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,17 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.FinalOSBlasting.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
     private EditText editTextUsername;
     private EditText editTextEmail;
     private EditText editTextPassword;
+
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,8 @@ public class LogIn extends AppCompatActivity {
         editTextUsername = findViewById(R.id.username_input);
         editTextEmail = findViewById(R.id.email_input);
         editTextPassword = findViewById(R.id.password_input);
+
+        auth = FirebaseAuth.getInstance();
 
         setContentView(com.example.FinalOSBlasting.R.layout.login_ui);
 
@@ -32,12 +41,19 @@ public class LogIn extends AppCompatActivity {
                 String username = editTextUsername.getText().toString();
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
+                loginUser(email, password);
 
-                if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LogIn.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LogIn.this, "Login successful", Toast.LENGTH_SHORT).show();
-                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LogIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LogIn.this, MainActivity.class));
+                finish();
             }
         });
     }
