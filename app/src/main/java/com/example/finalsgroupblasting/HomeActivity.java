e5c630fd6b8021c2162a75c99d6e33e76de25e9d
Kenzhi <ekce2252765@slu.edu.ph>
2025-12-09 12:28:16 +0800

    Fixes

--- app/src/main/java/com/example/finalsgroupblasting/HomeActivity.java
+++ app/src/main/java/com/example/finalsgroupblasting/HomeActivity.java
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.finalsgroupblasting.R;
 
 public class HomeActivity extends AppCompatActivity {
-//Lawyer Side UI
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/finalsgroupblasting/MainMenuUI.java
+++ app/src/main/java/com/example/finalsgroupblasting/MainMenuUI.java
@@ -8,12 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainMenuUI extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_ui);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
     }
 
 }
