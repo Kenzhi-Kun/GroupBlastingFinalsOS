4bd644b7a022f5a47687dbfa9b788050ea32ea23
Kenzhi <ekce2252765@slu.edu.ph>
2025-12-09 22:50:07 +0800

    Client Files

---
+++ app/src/main/java/com/example/finalsgroupblasting/ClientFiles.java
@@ -0,0 +1,66 @@
+package com.example.finalsgroupblasting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ClientFiles extends AppCompatActivity {
+
+    private TextView userNameTextView;
+    private ImageView uploadButton;
+    private ImageView filesButton;
+    private ImageView appointmentButton;
+    private TextView logoutTextBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_client_files);
+
+        appointmentButton = findViewById(R.id.appointment_home_client6);
+        uploadButton = findViewById(R.id.upload_home_client6);
+        filesButton = findViewById(R.id.files_home_client6);
+        logoutTextBtn = findViewById(R.id.logoutTextBtn6);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        appointmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ClientFiles.this, ClientAppointment.class);
+                startActivity(intent);
+            }
+        });
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent2 = new Intent(ClientFiles.this, ClientUpload.class);
+                startActivity(intent2);
+            }
+        });
+
+        logoutTextBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent4 = new Intent(ClientFiles.this, MainActivity.class);
+                startActivity(intent4);
+                finish();
+            }
+        });
+    }
+    }
+}
