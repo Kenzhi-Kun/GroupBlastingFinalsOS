032f1430426e046d068a3ca06c40dbccd40a5552
Kenzhi <ekce2252765@slu.edu.ph>
2025-12-10 00:58:01 +0800

    Client Side Fully functional

--- app/src/main/java/com/example/finalsgroupblasting/ClientUpload.java
+++ app/src/main/java/com/example/finalsgroupblasting/ClientUpload.java
@@ -1,6 +1,10 @@
 package com.example.finalsgroupblasting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ClientUpload extends AppCompatActivity {
+
+    private ImageView appointmentButton;
+    private ImageView uploadButton;
+    private ImageView filesButton;
+    private TextView logoutButton;
+    private TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_client_upload);
+
+        appointmentButton = findViewById(R.id.appointment_home_client3);
+        uploadButton = findViewById(R.id.upload_home_client3);
+        filesButton = findViewById(R.id.files_home_client3);
+        logoutButton = findViewById(R.id.logoutTextBtn3);
+        userName = findViewById(R.id.user_name7);
+        displayUsername();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,6 +43,49 @@ public class ClientUpload extends AppCompatActivity {
         });
 
 
+        filesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(ClientUpload.this, ClientFiles.class);
+                startActivity(intent2);
+                finish();
+            }
 
+        });
+
+        appointmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ClientUpload.this, ClientAppointment.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(ClientUpload.this, MainActivity.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
+
+    }
+
+    public void displayUsername() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            String userNameHere = user.getDisplayName();
+            if (userNameHere != null && !userNameHere.isEmpty()) {
+                userName.setText(userNameHere);
+            } else {
+                userName.setText("Anonymous");
+            }
+        } else {
+            userName.setText("Anonymous");
+        }
     }
 }
